648ec06b7f59ee5eaa1dba8381d19aee351a6175
ekta <ekta.bindal@techrbm.com>
2021-12-22 13:11:38 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import SalarySlip from './components/salaryslip';
+
+const App =() =>{
+  return (
+    <div className="App">
+      <header className="App-header">
+       <SalarySlip/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
